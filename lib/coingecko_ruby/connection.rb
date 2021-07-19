8f94a2d87fa15ca64ee9f9405f693f1741be8294
julianfoo <julian.foo@investingnote.com>
2021-07-19 21:53:19 +0800

    FIX: Fixed get call in connection.rb not passing query params correctly

--- lib/coingecko_ruby/connection.rb
+++ lib/coingecko_ruby/connection.rb
@@ -6,7 +6,7 @@ module CoingeckoRuby
   module Connection
     BASE_URL = 'https://api.coingecko.com/api/v3/'.freeze
 
-    def get(endpoint, params = {}, **options)
+    def get(endpoint, **params)
       url = BASE_URL + endpoint
       uri = URI(url)
       uri = build_request(uri, params) unless params.empty?
